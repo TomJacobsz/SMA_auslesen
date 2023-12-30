31a2a7d800bade8ad3d7a4f72f375d07cf504505
jacobst0 <s6tojaco@uni-bonn.de>
2023-12-30 16:40:35 +0100

    "Shelly auswerten und in Datenbank Schreiben"

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
+from flask import Flask, request, jsonify
 import mysql.connector
 from mysql.connector import Error
+
+app = Flask(__name__)
 
 params = {
     'host': 'dbtommi',
@@ -23,14 +26,26 @@ def read_database(query : str)->list:
             connection.close()
             print("MariaDB connection is closed")
 
-def pretty_print(last_day):
-    print("{:<30}{:<10}{:<10}{:<10}".format("Zeit","Leistung","Netzbezug","Ertrag"))
+def pretty_print(last_day : list)->list:
+    pretty_return = []
+    pretty_return.append("{:<30}{:<10}{:<10}{:<10}".format("Zeit","Leistung","Netzbezug","Ertrag"))
     for entry in last_day:
         datetime_obj, value1, value2, value3 = entry
         formatted_datetime = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
         formatted_entry = "{:<30}{:<10}{:<10}{:<10}".format(formatted_datetime, value1, value2, value3)
-        print(formatted_entry)
+        pretty_return.append(formatted_entry)
+    return pretty_return
 
-#last_day = read_database("SELECT * FROM Leistung where Zeit > DATE_SUB(NOW(), INTERVAL 10 MINUTE)")
-last_20_seconds = read_database("SELECT * FROM Leistung where Zeit > DATE_SUB(NOW(), INTERVAL 5 MINUTE)")
-pretty_print(last_20_seconds)
+
+@app.route('/PV/Leistung/last_2_minutes', methods=['GET'])
+def get_leistung():
+    last_2_minutes = read_database("SELECT * FROM Leistung where Zeit > DATE_SUB(NOW(), INTERVAL 2 MINUTE)")
+    return jsonify(last_2_minutes)
+
+@app.route('/PV/Leistung/last_day', methods=['GET'])
+def get_last_day():
+    last_day = read_database("SELECT * FROM Leistung where Zeit > DATE_SUB(NOW(), INTERVAL 1 DAY)")
+    return jsonify(pretty_print(last_day))
+
+if __name__ == '__main__':
+    app.run(debug=True)
