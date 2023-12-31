936cf8afda61181ad31ee0910f2891bebe52b0aa
TomJacobs <s6tojaco@uni-bonn.de>
2023-12-31 14:57:20 +0100

    Update and rename importrequests_run.sh to Wechselrichter_auslesen_run.sh
    umbennenung run_sh

---
+++ Wechselrichter_auslesen_run.sh
@@ -0,0 +1,8 @@
+sleep 20  # Warten Sie 20 Sekunden, um sicherzustellen, dass MariaDB bereit ist
+
+source /pfad/zum/env/bin/activate #pip env activieren
+
+python3 /pfad/zur/pythondatei/Wechselrichter_auslesen.py > /pfad/zur/pythondatei/Wechselrichter_auslesen.log 2>&1 #Logdatei für Autostart schreiben.
+
+
+# -> alternativ würde der Befehl /pfad/zum/env/python3 pfad/zur/pythondatei/Wechselrichter_auslesen.py auch funktionieren (denk ich)
